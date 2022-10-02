88e618b063b596ab4c96ec290104fe99e1b6a34a
alfre <alfredo.medina@uc.cl>
2022-10-01 21:45:04 -0300

    actuali

--- T2/artists.js
+++ T2/artists.js
@@ -1,4 +1,7 @@
 
+var CURRENT_CATEGORY = "";
+
+const selectOrder = document.getElementById("selector");
 const artistContainer = d3.select("#artists");
 // https://github.com/PUC-Infovis/codigos-2022-2/blob/main/Clase%2011%20-%20Utilidades%20D3%20I/programa_desarrollo_clases.js
 
@@ -15,10 +18,20 @@ function dataJoinArtist(datos) {
     }
 }
 
+function onlyFM(genderInput){
+    if (genderInput === 0){
+        artistContainer.selectAll(`.Male`).style("display", "none");
+        artistContainer.selectAll(`.Female`).style("display", "block");
+    }
+    else if (genderInput === 1){
+        artistContainer.selectAll(`.Female`).style("display", "none");
+        artistContainer.selectAll(`.Male`).style("display", "block");
+    }
+}
 
 
-function createSvgArtist(data, artworkScale, artistScale) {
-    const container = artistContainer.append("div").attr("class", "artist-container");
+function createSvgArtist(data) {
+    const container = artistContainer.append("div").attr("class", "artist-container "+ data.Gender);
     const title = container.append("h3").text(data.Artist).attr("class", "category-title");
 }
 
@@ -40,17 +53,38 @@ function createSvgArtist(data, artworkScale, artistScale) {
 
 // const parsingArtist = (d) => ({
 
-
-
-function runCodeArtist(category) {
+function selectorCode(){
+    if (CURRENT_CATEGORY === ""){
+        return
+    }
+    if (selectOrder.value === "Alphabetical"){
+        runCodeArtist(CURRENT_CATEGORY, true, false);
+    }
+    else if (selectOrder.value === "Age"){
+        runCodeArtist(CURRENT_CATEGORY, false, true);
+    }
+    else{
+        runCodeArtist();
+    }
+}
+function runCodeArtist(category=CURRENT_CATEGORY, orderAlpha=false, orderAge=false) {
+    // hacerlo con enter y exit
     artistContainer.selectAll(".artist-container").remove()
     const BASE_URL = "https://gist.githubusercontent.com/Hernan4444/";
     let URL = BASE_URL + "16a8735acdb18fabb685810fc4619c73/raw/d16677e2603373c8479c6535df813a731025fd4a/";
     URL = URL + "ArtistProcessed.csv";
-    console.log(URL);
     d3.csv(URL).then((data) => {
-      console.log(data);
+      CURRENT_CATEGORY = category;
       data = data.filter(d => d.Categories.includes(category));
+        if (orderAlpha) {
+            data = data.sort((a, b) => a.Artist.localeCompare(b.Artist));
+        }
+        console.log(data);
+        console.log(CURRENT_CATEGORY)
+        if (orderAge) {
+            data = data.sort((a, b) => parseInt(a.BirthYear) - parseInt(b.BirthYear)); // fix
+            console.log(data);
+        }
         dataJoinArtist(data);
     }).catch(error => {
       console.log(error);
