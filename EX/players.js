49ae32a02cd9146d5e37d5306c5ba69035d1de03
alfre <alfredo.medina@uc.cl>
2022-11-05 20:30:10 -0300

    avacnes

--- EX/players.js
+++ EX/players.js
@@ -1,33 +1,178 @@
 
 const mainContainer = d3.select("#main-container");
+const playersContainer = d3.select("#players-container");
 
 
 function dataJoinPlayers(datos) {
     // Vinculamos los datos con cada elemento rect con el comando join
-    const container = mainContainer.append("svg")
+    const container = playersContainer.append("div")
+
     maximo = d3.max(datos, d => d.rating_standard);
     minimo = d3.min(datos, d => d.rating_standard);
+    container
+        .attr("class", "container")
     const escalaAltura = d3.scaleLinear()
-        .domain([0, maximo])
-        .range([0, 500])
-    const escalaEjeY = d3.scaleLinear()
-        .domain([0, maximo])
-        .range([500, 0])
-    const escalaBarras = d3.scaleBand()
-        .domain(datos.map(d => d.name))
-        .range([0, 400])
-        .padding(0.1);
+        .domain([minimo, maximo])
+        .rangeRound([70, 150])
 
     const enter_and_update = container
-        .selectAll("rect")
+        .selectAll("div")
         .data(datos)
-        .join("rect")
-    enter_and_update
-        .attr("width", escalaBarras.bandwidth())
-        .attr("fill", "orange")  
-        .attr("height", (d) => escalaAltura(d.rating_standard)) //Aplicamos nuestra escala
-        .attr("y", (d) => escalaEjeY(d.rating_standard)) //Aplicamos nuestra escala
-        .attr("x", (d) => escalaBarras(d.name));
+        .join("div");
+    // enter_and_update
+    //     .attr("x", (d, i) => (i%8)*75)
+    //     .attr("y", (d, i) => 200-escalaAltura(d.rating_standard)+Math.floor(i/8)*110);
+    kingsvg(enter_and_update, escalaAltura);
+    // tooltip(enter_and_update);
+    enter_and_update.on("mouseover", function(d) {
+        d3.selectAll(".king").style("opacity", "0.6");
+        d3.select(this).select(".king").style("opacity", "1");
+        // d3.select(this).select(".tooltip").style("visibility", "visible");
+    })
+    .on("mouseout", function(d) { 
+        d3.selectAll(".king").style("opacity", "1");
+        // d3.select(this).select(".tooltip").style("visibility", "hidden");
+    });
+    
+    // enter_and_update.append("image")
+    //     .attr("xlink:href", d => flagSvg(d.federation))
+    //     .attr("width", 25)
+    //     .attr("height", 25)
+    //     .attr("x", (d) => escalaX(d.name)%2)
+    //     .attr("y", (d) => Math.floor(escalaX(d.name)/2))
+}
+
+// function tooltip(container){
+//     const tooltip = container.append("div")
+//         .attr("class", "tooltip")
+//         .attr("x", 0)
+//         .attr("y", 0)
+    
+//     const nameflag = tooltip.append("div")
+//         .attr("class", "nameflag")
+//     const svg = nameflag.append("svg")
+//         .attr("width", 25)
+//         .attr("height", 25)
+//     svg.append("image")
+//         .attr("xlink:href", d => flagSvg(d.federation))
+//         .attr("width", 25)
+//         .attr("height", 25);
+//     nameflag.append("div")
+//         .text((d) => textTooltip(d)[0])
+//     tooltip.append("div")
+//         .text((d) => textTooltip(d)[1])
+//     tooltip.append("div")
+//         .text((d) => textTooltip(d)[2])
+//     tooltip.append("div")
+//         .text((d) => textTooltip(d)[3]);
+
+// }
+function textTooltip(data) {
+    const text = data.name + "\n" + "\n" + 
+        "Classic Rating: " + data.rating_standard + "\n" +
+        "Rapid Rating: " + data.rating_rapid + "\n" +
+        "Blitz Rating: " + data.rating_blitz;
+    return text
+}
+
+
+function posicionRect(d, escala) {
+    return 154-escala(d.rating_standard)
+}
+
+function kingsvg(svg, escalaAltura){
+    // svg del rey
+    const color = "#383838";
+    // tronco del rey
+    const rey = svg.append("svg")
+        .attr("width", 100).attr("class", "king").attr("style", "cursor: pointer;")
+        .attr("height", 200)
+    
+    rey.append("title").text(d => textTooltip(d))
+    
+    rey.append("rect")
+        .attr("width", 20)
+        .attr("height", (d) => escalaAltura(d.rating_standard))
+        .attr("fill", color)
+        .attr("y", (d) => posicionRect(d, escalaAltura))
+        
+    rey.append("rect")
+        .attr("width", 5)
+        .attr("height", 20)
+        .attr("fill", color)
+        .attr("x", 7.5)
+        .attr("y", (d, i) => posicionRect(d, escalaAltura) - 20);
+    rey.append("rect")
+        .attr("width", 20)
+        .attr("height", 5)
+        .attr("fill", color)
+        .attr("x", 0)
+        .attr("y", (d, i) => posicionRect(d, escalaAltura) - 12);
+    
+    // cuello del rey
+    rey.append("polygon")
+        .attr("points", (d) => `-5,${posicionRect(d, escalaAltura)} 25,${posicionRect(d, escalaAltura)} 10,${posicionRect(d, escalaAltura)+60}`)
+        .attr("fill", color)
+        .attr("x", -5)
+        .attr("y", (d, i) => posicionRect(d, escalaAltura) - 20);
+    rey.append("rect")
+        .attr("width", 40)
+        .attr("height", 5)
+        .attr("fill", color)
+        .attr("x", -10)
+        .attr("y", (d, i) => posicionRect(d, escalaAltura) + 20);
+    rey.append("rect")
+        .attr("width", 30)
+        .attr("height", 5)
+        .attr("fill", color)
+        .attr("x", -5)
+        .attr("y", (d, i) => posicionRect(d, escalaAltura) + 25);
+    
+    // base del rey
+    rey.append("rect")
+        .attr("width", 40)
+        .attr("height", 5)
+        .attr("fill", color)
+        .attr("x", -10)
+        .attr("y", (d) => posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)- 5);
+    rey.append("rect")
+        .attr("width", 35)
+        .attr("height", 15)
+        .attr("fill", color)
+        .attr("x", -7.5)
+        .attr("y", (d) => posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)- 15);
+    rey.append("rect")
+        .attr("width", 30)
+        .attr("height", 5)
+        .attr("fill", color)
+        .attr("x", -5)
+        .attr("y", (d) => posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)- 20);
+    rey.append("polygon")
+        .attr("points",(d) => `-2,${posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)-20} 22,${posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)-20} 10,${posicionRect(d, escalaAltura)}`)
+        .attr("fill", color)
+        .attr("x", -5)
+        .attr("y", (d) => posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)- 20);
+
+    rey.append("image")
+        .attr("xlink:href", d => flagSvg(d.federation))
+        .attr("width", 25)
+        .attr("height", 25)
+        .attr("x", -2.5)
+        .attr("y", (d) => posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)+5);
+        ;
+    rey.append("text")
+        .attr("x", -20)
+        .attr("y", (d) => posicionRect(d, escalaAltura)+escalaAltura(d.rating_standard)+ 50)
+        .text((d) => textPlayer(d.name))
+
+}
+
+
+function textPlayer(name){
+    if (name.length > 10){
+        return name.substring(0, 7) + "...";
+    }
+    return name
 }
 
 function dataJoinGraph(datos) {
@@ -145,17 +290,24 @@ const parseData = (d) => ({
   });
 
 
+function flagSvg(flag) {
+    return "https://ratings.fide.com/svg/" + flag + ".svg";
+}
+
+
 function runCode() {
     // https://www.kaggle.com/datasets/rohanrao/chess-fide-ratings
     const BASE_URL = "https://gist.githubusercontent.com/AlfreMK/";
     let URL = BASE_URL + "a2ea95d3edc1de632237cd4c2ae0a8f8/raw/9ab4b21d70baa0683428e6bbd6f8262242b7e869/";
     URL = URL + "fide_data_01_2021.csv";
     d3.csv(URL, parseData).then((data) => {
-        // data = data.filter(d => Math.random() > 0.7).slice(0, 100000);
+        // sort descending
+        data_players = data.sort((a, b) => b.rating_standard - a.rating_standard);
+        data_players = data_players.slice(0, 10);
+        dataJoinPlayers(data_players);
         data = divide_players_into_ranges(data);
         data = data.sort((a, b) => a.min - b.min);
         dataJoinGraph(data);
-        // dataJoinPlayers(data);
     }).catch(error => {
       console.log(error);
     })
